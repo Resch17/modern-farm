c6f48eb2a199c1dc26a7afc20725367f6d0b04da
Aaron Resch <aaronmresch@gmail.com>
2020-11-18 13:50:16 -0600

    added an equals sign to isArray check (lookin' for corn)

--- src/scripts/field.js
+++ src/scripts/field.js
@@ -1,7 +1,7 @@
 let plantArray = [];
 
 export const addPlant = (seed) => {
-  if (Array.isArray(seed) == true) {
+  if (Array.isArray(seed) === true) {
     for (let i = 0;i<seed.length;i++) {
       plantArray.push(seed[i]);
     }
