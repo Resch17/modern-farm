339ffc88972ff23b2b06ee9d0b68a20125b2c049
Aaron Resch <aaronmresch@gmail.com>
2020-11-21 15:55:24 -0600

    changed some css, changed catalog.js to use a .forEach method instead of a for--of loop

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -1,7 +1,7 @@
 export const Catalog = (foodArray) => {
   const htmlContainer = document.querySelector(".container");
 
-  for (const item of foodArray) {
+  foodArray.forEach(item => {
     htmlContainer.innerHTML += `<section class="plant ${item.type}"><img src="/./images/${item.type}.png"><p>${item.type}</p></section>`;
-  }
+  })
 };
